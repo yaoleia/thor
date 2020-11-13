d4959089cba1d464dce2ac1813190b72e2260cd7
姚磊 <yaolei@ainnovation.com>
2020-11-13 12:07:16 +0800

    feat: 本地图片转存

--- app/service/file.js
+++ app/service/file.js
@@ -9,16 +9,28 @@ const pump = require('mz-modules/pump')
 
 class FileService extends Service {
   async upload({ file, image_url, type }) {
-    const suffix = path.basename(image_url)
-    const hash = crypto.createHash('md5')
-    const now = type || (new Date()).getTime()
-    const fileName = hash.update(now + Math.random().toString()).digest('hex') + suffix
-
-    const date = moment().format('YYYY/MM/DD')
-    const uploadDir = path.resolve(this.app.config.UPLOAD_DIR, date)
-    if (!fs.existsSync(uploadDir)) mkdirp.sync(uploadDir)
-    await pump(request(image_url), fs.createWriteStream(path.join(uploadDir, fileName)))
-    return new URL(path.join('public/upload', date, fileName), this.ctx.request.origin).href;
+    let hasImage = false
+    if (image_url) {
+      const suffix = path.basename(image_url)
+      const hash = crypto.createHash('md5')
+      const now = type || (new Date()).getTime()
+      const fileName = hash.update(now + Math.random().toString()).digest('hex') + suffix
+      const date = moment().format('YYYY/MM/DD')
+      const uploadDir = path.resolve(this.app.config.UPLOAD_DIR, date)
+      if (!fs.existsSync(uploadDir)) mkdirp.sync(uploadDir)
+      if (image_url.startsWith('http')) {
+        await pump(request(image_url), fs.createWriteStream(path.join(uploadDir, fileName)))
+        hasImage = true
+      } else {
+        const exists = fs.existsSync(image_url)
+        if (exists) {
+          fs.copyFileSync(image_url, path.join(uploadDir, fileName))
+          hasImage = true
+        }
+      }
+      if (hasImage) return new URL(path.join('public/upload', date, fileName), this.ctx.request.origin).href;
+    }
+    return ''
   }
 }
 
