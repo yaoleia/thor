4ebb3c40c219d6ed344b9802fb734990abb6889f
姚磊 <yaolei@ainnovation.com>
2020-11-16 19:15:04 +0800

    fix: ws输出设备ID

--- app/controller/pusher.js
+++ app/controller/pusher.js
@@ -5,22 +5,23 @@ const Controller = require('egg').Controller;
 class PusherController extends Controller {
   async index() {
     const { ctx } = this;
-    const { device, image_url } = ctx.request.body
+    const { device_id, image_url } = ctx.request.body
     const date = ctx.helper.getDate()
-    const [{ defect_list }, [image]] = await Promise.all([
+    const [{ defect_items }, [image]] = await Promise.all([
       ctx.service.backend.defect(image_url),
       ctx.service.file.upload(ctx)
     ])
     const defectData = {
-      is_defect: !!_.get(defect_list, 'length'),
-      defect_list,
+      is_defect: !!_.get(defect_items, 'length'),
+      defect_items,
+      device_id,
       image_url,
       thumbnail_url: _.get(image, 'url'),
       date
     }
     ctx.logger.debug(defectData)
     const nsp = this.app.io.of('/')
-    const room = device ? nsp.to(device) : nsp
+    const room = device_id ? nsp.to(device_id) : nsp
     room.emit('res', defectData)
     ctx.body = defectData
   }
