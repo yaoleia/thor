2747fad0e0546ff1f3c57cea89a1b0ccd7274ce7
姚磊 <yaolei@ainnovation.com>
2020-12-02 15:38:38 +0800

    feat: 获取brpop超时重发, retry次数配置

--- agent.js
+++ agent.js
@@ -14,20 +14,39 @@ module.exports = agent => {
 module.exports = agent => {
   const { CONCURRENT = 1 } = agent.config
   const msgMap = {}
-  function startWaitMsg(key) {
+
+  function stopWaitMsg(device_id) {
+    if (!msgMap[device_id]) return
+    msgMap[device_id].switch = false
+    delete msgMap[device_id]
+  }
+
+  function startWaitMsg(device_id) {
+    const key = `list#${device_id}`
     const listen = { switch: true }
+    let retry = 10
     new Array(CONCURRENT).fill().forEach(async _ => {
       while (listen.switch) {
         try {
-          const resp = await agent.redis.get('mq').brpop(key, 600)
+          const redis = agent.redis.get('mq')
+          const resp = await redis.brpop(key, 600)
           // 随机给一个worker消费一条队列信息
-          if (!resp || !resp[1]) continue
-          const msg = JSON.parse(resp[1])
-          agent.messenger.sendRandom('handle_msg', msg)
+          if (!resp || !resp[1]) throw 'timeout or data error!'
+          if (!listen.switch) {
+            await redis.lpush(...resp)
+            throw 'closed listen, pass this msg!'
+          }
           await new Promise((resolve, reject) => {
+            retry = 10
+            const msg = JSON.parse(resp[1])
+            agent.messenger.sendRandom('handle_msg', msg)
             agent.messenger.once(msg.uid, resolve)
           })
         } catch (error) {
+          if (--retry < 0) {
+            stopWaitMsg(device_id)
+            break
+          }
           continue
         }
       }
@@ -37,14 +56,10 @@ module.exports = agent => {
 
   agent.messenger.on('wait_msg', device_id => {
     if (msgMap[device_id]) return
-    msgMap[device_id] = startWaitMsg(`list#${device_id}`)
+    msgMap[device_id] = startWaitMsg(device_id)
   })
 
-  agent.messenger.on('stop_wait', device_id => {
-    if (!msgMap[device_id]) return
-    msgMap[device_id].switch = false
-    delete msgMap[device_id]
-  })
+  agent.messenger.on('stop_wait', stopWaitMsg)
 
   agent.messenger.once('egg-ready', async () => {
     agent.messenger.sendRandom('init-event')
