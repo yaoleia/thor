4efe094b525f2be64f9b2597dfd696446228c296
姚磊 <yaolei@ainnovation.com>
2020-12-23 16:13:58 +0800

    feat: 生产记录多条件筛选增加indexes

--- app/model/record.js
+++ app/model/record.js
@@ -4,16 +4,16 @@ module.exports = ({ mongoose, config }) => {
   const { baseUrl } = config.static
   const RecordSchema = new mongoose.Schema({
     uid: { type: String, default: uuidv4, unique: true },
-    time: { type: Date, default: Date.now, get: v => v && v.valueOf() },
+    time: { type: Date, index: -1, default: Date.now, get: v => v && v.valueOf() },
     device: {
-      uid: { type: String },
+      uid: { type: String, index: true },
       name: { type: String },
       camera_server: { type: String },
       model_server: { type: String },
       ip: { type: String }
     },
     pattern: {
-      uid: { type: String },
+      uid: { type: String, index: true },
       name: { type: String },
       size_model: { type: String, set: v => urlFilter(v, baseUrl), get: v => urlJoin(v, baseUrl) },
       size_md5: { type: String },
@@ -30,9 +30,34 @@ module.exports = ({ mongoose, config }) => {
   }, {
     versionKey: false
   })
-  RecordSchema.index({ time: -1 })
-  RecordSchema.index({ size_alarm: 1, defect_alarm: 1 })
-  RecordSchema.index({ time: -1, size_alarm: 1, defect_alarm: 1 })
+
+  RecordSchema.index({ time: -1, "device.uid": 1 })
+  RecordSchema.index({ time: -1, "pattern.uid": 1 })
+  RecordSchema.index({ time: -1, defect_alarm: 1 })
+  RecordSchema.index({ time: -1, size_alarm: 1 })
+  RecordSchema.index({ "device.uid": 1, "pattern.uid": 1 })
+  RecordSchema.index({ "device.uid": 1, defect_alarm: 1 })
+  RecordSchema.index({ "device.uid": 1, size_alarm: 1 })
+  RecordSchema.index({ "pattern.uid": 1, defect_alarm: 1 })
+  RecordSchema.index({ "pattern.uid": 1, size_alarm: 1 })
+  RecordSchema.index({ defect_alarm: 1, size_alarm: 1 })
+  RecordSchema.index({ time: -1, "device.uid": 1, "pattern.uid": 1 })
+  RecordSchema.index({ time: -1, "pattern.uid": 1, defect_alarm: 1 })
+  RecordSchema.index({ time: -1, defect_alarm: 1, size_alarm: 1 })
+  RecordSchema.index({ time: -1, "device.uid": 1, defect_alarm: 1 })
+  RecordSchema.index({ time: -1, "device.uid": 1, size_alarm: 1 })
+  RecordSchema.index({ time: -1, "pattern.uid": 1, size_alarm: 1 })
+  RecordSchema.index({ "device.uid": 1, "pattern.uid": 1, defect_alarm: 1 })
+  RecordSchema.index({ "device.uid": 1, defect_alarm: 1, size_alarm: 1 })
+  RecordSchema.index({ "device.uid": 1, "pattern.uid": 1, size_alarm: 1 })
+  RecordSchema.index({ "pattern.uid": 1, defect_alarm: 1, size_alarm: 1 })
+  RecordSchema.index({ time: -1, "device.uid": 1, "pattern.uid": 1, defect_alarm: 1 })
+  RecordSchema.index({ time: -1, "device.uid": 1, "pattern.uid": 1, size_alarm: 1 })
+  RecordSchema.index({ time: -1, "device.uid": 1, defect_alarm: 1, size_alarm: 1 })
+  RecordSchema.index({ time: -1, "pattern.uid": 1, defect_alarm: 1, size_alarm: 1 })
+  RecordSchema.index({ "device.uid": 1, "pattern.uid": 1, defect_alarm: 1, size_alarm: 1 })
+  RecordSchema.index({ time: -1, "device.uid": 1, "pattern.uid": 1, defect_alarm: 1, size_alarm: 1 })
+
   RecordSchema.plugin(mongooseLeanGetters)
   RecordSchema.set('toObject', { getters: true })
   return mongoose.model('Record', RecordSchema);
